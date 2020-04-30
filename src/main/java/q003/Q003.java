ce1eabbbf1585cfcc0a67c1bd41824ab351ef38d
Kazuma Kikuchi <63339015+kazu-kichi@users.noreply.github.com>
2020-04-30 17:42:58 +0900

    q003 fixed

--- src/main/java/q003/Q003.java
+++ src/main/java/q003/Q003.java
@@ -1,6 +1,15 @@
 package q003;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * Q003 集計と並べ替え
@@ -32,6 +41,46 @@ public class Q003 {
      */
     private static InputStream openDataFile() {
         return Q003.class.getResourceAsStream("data.txt");
+    }
+
+    private static Optional<Map<String, Integer>> read(InputStream inputStream) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inputStream, Charset.forName("UTF-8")))) {
+            Map<String, Integer> result = new HashMap<>();
+
+            reader.lines()
+                    .forEach(line ->
+                            Arrays.stream(line.split("[ ,.;]"))
+                                    .filter(s -> !s.isEmpty())
+                                    .map(v -> {
+                                        if (!"I".equals(v)) {
+                                            return v.toLowerCase();
+                                        }
+                                        return v;
+                                    })
+                                    .forEach(v -> {
+                                        if (result.containsKey(v)) {
+                                            int count = result.get(v);
+                                            result.put(v, ++count);
+                                        } else {
+                                            result.put(v, 1);
+                                        }
+                                    })
+                    );
+
+            return Optional.of(result);
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+    }
+
+    public static void main(String[] args) {
+        read(openDataFile())
+                .ifPresent(v ->
+                        v.entrySet().stream()
+                                .sorted(Comparator.comparing(Map.Entry::getKey, String.CASE_INSENSITIVE_ORDER))
+                                .forEach(System.out::println)
+                );
     }
 }
 // 完成までの時間: xx時間 xx分
